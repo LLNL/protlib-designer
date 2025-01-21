b61475866cb3c9da67f4c66234fc1a72706a90f6
Mikel Landajuela Larma <landajuelala1@llnl.gov>
2025-01-20 23:17:59 -0800

    Update setup

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
 
-setup(entry_points={"console_scripts": ["protlib-designer=scripts.run_protlib_designer:run_protlib_designer"]})
+setup(
+    entry_points={
+        "console_scripts": [
+            "protlib-designer=scripts.run_protlib_designer:run_protlib_designer",
+            "protlib-plm-scorer=scripts.run_plm_scorer:run_plm_scorer",
+        ]
+    }
+)
