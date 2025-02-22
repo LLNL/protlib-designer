d069700f2ee89cc369721c62ec3a71c57e566099
Mikel Landajuela Larma <landajuelala1@llnl.gov>
2025-02-21 17:02:57 -0800

    Add version control

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 
 setup(
+    use_scm_version={"write_to": "protlib_designer/_version.py"},
+    setup_requires=['setuptools_scm'],
     entry_points={
         "console_scripts": [
             "protlib-designer=scripts.run_protlib_designer:run_protlib_designer",
